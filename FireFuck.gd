be21c19aa10b0b96892fcb29e66757c451240a8d
AnnieGreenfield <annipap@yandex.by>
2023-11-29 14:18:14 +0300

    Uiiihhaha

---
+++ FireFuck.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		Game.playerHP = 0 # Replace with function body.
--- Levels.gd
+++ Levels.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_level_1_pressed():
-	get_tree().change_scene_to_file("res://level1.tscn")
+	get_tree().change_scene_to_file("res://Level_1.tscn")
 
 
 func _on_level_2_pressed():
